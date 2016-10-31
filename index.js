e548e681fc2d20b3bd85df85e103d24306c1a676
Orest Prystayko <orestprustayko@gmail.com>
2016-10-31 21:46:02 +0200

    Update index.js
    add additional checking to require `angular.js`

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //https://github.com/angular/angular.js/pull/10732
 
-var angular = require('angular');
+if (typeof angular !== 'undefined') {
+  var angular = require('angular');
+}
 var mask = require('./dist/mask');
 
 module.exports = 'ui.mask';
